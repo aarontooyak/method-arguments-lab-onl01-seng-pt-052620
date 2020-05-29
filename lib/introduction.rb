0f9386c79c95af0c1620df27327a25d83808a634
Aaron Tooyak <tooyak.aaron@gmail.com>
2020-05-29 03:45:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,12 +1,10 @@
 def 
   introduction(name)
-    name = Aaron
     puts "Hi, my name is #{name}."
 end
 
 def 
   introduction_with_language(language,name)
-    language = Ruby
     puts "Hi, my name is #{name}and I am learning to program in #{language}."
   end
   
